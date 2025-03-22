d09a0a7d9a5415a3bd2289132ad4d34c4a32ca8f
George Jacobs <george@r2d2>
2025-03-22 16:12:25 -0400

    update formatting

--- iam.tf
+++ iam.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "role" {
-  name = "sanitize_data_lambda_role"
+  name               = "sanitize_data_lambda_role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
 }
 
@@ -8,7 +8,7 @@ data "aws_iam_policy_document" "assume_role" {
     effect = "Allow"
 
     principals {
-      type = "Service"
+      type        = "Service"
       identifiers = ["lambda.amazonaws.com"]
     }
 
@@ -17,11 +17,11 @@ data "aws_iam_policy_document" "assume_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "lambda" {
-  role = aws_iam_role.role.name
+  role       = aws_iam_role.role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
 resource "aws_iam_role_policy_attachment" "s3" {
-  role = aws_iam_role.role.name
+  role       = aws_iam_role.role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
 }
